639f1bcd54c4edcb223a3ff88d2c887c49a27804
Anon Bianco <anonh7@gmail.com>
2023-05-22 12:50:45 +0530

    Update Main.java

--- PP/fibonacci series/src/vovels/Main.java
+++ PP/fibonacci series/src/vovels/Main.java
@@ -1,19 +1,26 @@
 package vovels;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		int n1=0,n2=1,n3,i,count=10;    
-		 System.out.print(n1+" "+n2); 
-		    
-		 for(i=2;i<count;++i)   
-		 {    
-		  n3=n1+n2;    
-		  System.out.print(" "+n3);    
-		  n1=n2;    
-		  n2=n3;    
-		 }    
+		int n1 = 0, n2 = 1; 
+		
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the no of digits to be printed: ");
+		int count = sc.nextInt();
+
+		System.out.print(n1 + " " + n2);
+		
+		for(int i = 3; i<=count; i++) {
+			int n3 = n1 + n2;
+			System.out.print(" " + n3);
+			n1 = n2;
+			n2 = n3;
+		}
 
 	}
 
