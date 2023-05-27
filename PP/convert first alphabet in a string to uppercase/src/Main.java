aceabdc507b8dd4ad488f01ba41092b3fc0d413c
Anon Bianco <anonh7@gmail.com>
2023-05-27 21:56:31 +0530

    a

---
+++ PP/convert first alphabet in a string to uppercase/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String a = "hello";
+		
+//		char[] ca = a.toCharArray();
+//		
+//		ca[0] = (char)(ca[0] - 32);
+//		
+//		String b = new String(ca);
+//		
+//		System.out.println(b);
+		
+		System.out.println(a.substring(0, 1).toUpperCase() + a.substring(1, 5));
+
+	}
+
+}
